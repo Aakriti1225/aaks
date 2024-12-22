35fdd1a6b1cf29e82f44806904ce0cfb9d6f7af3
(aakriti) <aakritichitrakar@gmail.com>
2024-12-22 08:35:32 +0545

    first html class

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let name = "Aakriti";
+function sayhello(){
+    console.log("hello",+name);
+}
+{
+          let fruits=["Apple","kiwi"]
+          fruits.push("orage");
+          console.log(fruits)
+}
+{
+    let a=["hi","my","name","is","Aakriti"];
+    let b=a.join(" ");
+    console.log(b); 
+}
+
+    function learnNEw() 
+{
+    console.log("git revert new class"); 
+}
